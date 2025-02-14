22729e7705da295059a09b985a4e9cad38615fb1
John Michael Maala <113663564+apcjomaala1@users.noreply.github.com>
2025-02-14 00:03:20 +0000

    import personal profile into component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <h1>Food</h1>
+  <personal-profile/> <br>
   <food-item/> <br>
   <food-item2/> <br>
   <food-item/> <br>
   <food-item2/> <br>
 </template>
 
-<script></script>
+<script>
+</script>
 
 <style>
 
---
+++ vue-project/src/components/PersonalProfile.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="container">
+<head>
+    <title>Personal Profile of John Michael Maala</title>
+</head>
+<body>
+    <h1>About Me</h1>
+    <p>My name is John Michael Maala, also known as Michael or JM. I am a student with a keen interest in technology, inspired by my father’s printing business.</p>
+    
+    <h2>Education</h2>
+    <ul>
+        <li><strong>High School:</strong> Pasay City National Science High School</li>
+        <li><strong>College:</strong> BS Computer Science - Specialization in Software Systems (BSCS-SS)</li>
+    </ul>
+
+    <h2>IT Experience</h2>
+    <ul>
+        <li>Programming</li>
+        <li>Gaming (avid gamer)</li>
+        <li>Helping in my father's printing business</li>
+    </ul>
+
+    <h2>Hobbies & Interests</h2>
+    <ul>
+        <li>Playing games (simulation mostly)</li>
+        <li>Playing the guitar (beginner)</li>
+        <li>Making music</li>
+        <li>Taking care of my cute cat</li>
+    </ul>
+
+    <h2>Goals in Life / Dream</h2>
+    <table>
+        <tr>
+            <th>Goals</th>
+            <th>Dreams</th>
+        </tr>
+        <tr>
+            <td>Accomplish financial stability and make a lot of money to create my own game</td>
+            <td>Develop my own game with significant financial resources</td>
+        </tr>
+    </table>
+
+    <h2>Picture Gallery</h2>
+    <ul>
+        <li><img src="cat.jpg" alt="Cat" width="200"></li>
+        <li><img src="cat_2.jpg" alt="Cat 2" width="200"></li>
+    </ul>
+
+    <h2>Links</h2>
+    <ul>
+        <li><a href="https://github.com/apcjomaala1">GitHub</a></li>
+    </ul>
+
+</body>
+</div>
+</template>
